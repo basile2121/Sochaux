d49ba69d3e486ae78dd2ec724c8d0ecebbf70162
basile2121 <basile.regnault@gmail.com>
2021-03-09 18:41:31 +0100

    Ajout du convertisseur en PDF d'un rapport Specifique (Robin) + modification pour Bug

--- src/Controller/RapportSpecifiqueController.php
+++ src/Controller/RapportSpecifiqueController.php
@@ -4,6 +4,9 @@
 namespace App\Controller;
 
 
+namespace App\Controller;
+define("FPDF_FONTPATH","fpdf/font/");
+require("../fpdf/fpdf.php");
 use App\Data\SearchData;
 use App\Entity\Notification;
 use App\Entity\RapportSpecifique;
@@ -11,6 +14,7 @@ use App\Form\RapportSpecifiqueType;
 use App\Form\RapportsSearchType;
 use App\Repository\NotificationRepository;
 use App\Repository\RapportSpecifiqueRepository;
+use phpDocumentor\Reflection\Types\String_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -120,5 +124,137 @@ class RapportSpecifiqueController extends AbstractController
         }
 
         return $this->redirectToRoute('rapportSpecifiques_show');
+    }
+
+    /**
+     * @Route("/rapportSpecifiques/genereRapportPDF/{id}" , name="genereRapportPDF", methods={"GET"})
+     * @throws \Exception
+     */
+    public function genereRapportPDF(RapportSpecifique $rapportSpecifique): Response
+    {
+        $pdf = new \FPDF("P","pt","A4");
+        $pdf ->Open(); //indique que l'on crée un fichier PDF
+        $pdf ->AddPage(); //permet d'ajouter une page
+        $pdf ->SetFont('Helvetica','B',11); //choix de la police
+        $pdf ->SetXY(330, 25); // indique des coordonnées pour
+        $image1 = "../public/images/logo_fc_sochaux.jpg";
+        $pdf->Cell( 100, 100, $pdf->Image($image1, -10, 10, 150), 0, 0, 'L', false );
+        $prenom = $rapportSpecifique->getJoueur()->getPrenom();
+        $nom =  $rapportSpecifique->getJoueur()->getNom();
+        $titre= $nom ." ". $prenom;
+        $pdf->SetFont('Arial','B',15);
+        $w=$pdf->GetStringWidth($titre)-6;
+        $pdf->SetX(($w+400)/2);
+        $pdf->SetFillColor(230,230,0);
+        $pdf->SetLineWidth(1);
+        $pdf->SetX(200);
+        $pdf->Cell(200,45,$titre,1,1,'C',1);
+        $pdf->SetFont('Arial','',12);
+        $pdf->Ln(10);
+        $pdf->SetTitle($titre);
+
+        $pdf->SetY(100);
+        $pdf->SetFont('Arial','B',12);
+        $pdf->SetX(140);
+        $pdf->Cell(330,25,("Informations de l'agent ".$rapportSpecifique->getNomAgent()),0,0,'C',0);
+
+        $pdf->SetFont('Arial','',12);
+        $pdf->Ln();
+        $pdf->SetX(70);
+        $pdf->Cell(188,40,utf8_decode("Mail Agent"),1,0,'C',0);
+        $pdf ->Cell(288,40,utf8_decode($rapportSpecifique->getMailAgent()),1,0,'C',0); //insertion d'une ligne
+        $pdf->Ln();
+        $pdf->SetX(70);
+        $pdf->Cell(188,40,utf8_decode("Telephone Agent"),1,0,'C',0);
+        $pdf ->Cell(288,40,utf8_decode( $rapportSpecifique->getTelephoneAgent()),1,0,'C',0); //insertion d'une ligne
+        $pdf->Ln();
+        $pdf->SetX(70);
+        $pdf->Cell(188,40,utf8_decode("Adresse Agent"),1,0,'C',0);
+        $pdf ->Cell(288,40,utf8_decode( $rapportSpecifique->getAdresseAgent()),1,0,'C',0); //insertion d'une ligne
+        $pdf->Ln();
+
+
+        $pdf->SetY(270);
+        $pdf->SetFont('Arial','B',12);
+        $pdf->SetX(140);
+        $pdf->Cell(330,25,("Le rapport specifique de ".$rapportSpecifique->getJoueur()),0,0,'C',0);
+
+        $pdf->SetFont('Arial','',12);
+        $pdf->SetY(305);
+        $pdf->SetX(70);
+        $pdf->Cell(188,40,"Date Rapport",1,0,'C',0);
+        $date = $rapportSpecifique->getDateRapport()->format('d/m/Y');
+        $pdf ->Cell(288,40, $date,1,0,'C',0); //insertion d'une ligne
+        $pdf->Ln();
+        $pdf->SetX(70);
+        $pdf->Cell(188,40,utf8_decode("Match"),1,0,'C',0);
+        $pdf ->Cell(288,40, utf8_decode($rapportSpecifique->getEquipe1()." - ".$rapportSpecifique->getEquipe2()),1,0,'C',0); //insertion d'une ligne
+        $pdf->Ln();
+        $pdf->SetX(70);
+        $pdf->Cell(188,40,utf8_decode("Note Du joueur"),1,0,'C',0);
+        $pdf ->Cell(288,40, utf8_decode($rapportSpecifique->getNoteJoueur())."/20",1,0,'C',0); //insertion d'une ligne
+        $pdf->Ln();
+
+        $pdf->SetY(450);
+        $pdf->SetFont('Arial','B',12);
+        $pdf->SetX(140);
+        $pdf->Cell(330,25,"Les Qualites du Joueur" , 0,0,'C',0);
+        $pdf->SetFont('Arial','',12);
+        $pdf->SetY(500);
+        $pdf->SetX(70);
+
+        $technique = $rapportSpecifique->getQualiteTechnique();
+        $tailleTechnique = strlen($technique);
+        if ($tailleTechnique < 20){
+            $pdf->Cell(188,40  ,utf8_decode("Qualité Technique"),1,0,'D',0);
+            $pdf->MultiCell(288,40, utf8_decode($technique),1,'D',0);
+        }else{
+            $pdf->Cell(188,40  ,utf8_decode("Qualité Technique"),1,0,'D',0);
+            $pdf->MultiCell(288,23, utf8_decode($rapportSpecifique->getQualiteTechnique()),1,'D',0);
+        }
+
+        $pdf->Ln();
+        $pdf->SetX(70);
+
+        $physique = $rapportSpecifique->getQualitePhysique();
+        $taillePhysique = strlen($physique);
+        if ($taillePhysique < 50){
+            $pdf->Cell(188,40,utf8_decode("Qualité Physique"),1,0,'D',0);
+            $pdf->MultiCell(288,40, utf8_decode($physique),1,'D',0);
+        }else{
+            $pdf->Cell(188,40,utf8_decode("Qualité Physique"),1,0,'D',0);
+            $pdf->MultiCell(288,23, utf8_decode($physique),1,'D',0);
+        }
+
+
+        $pdf->Ln();
+        $pdf->SetX(70);
+
+        $tactique = $rapportSpecifique->getQualiteTactique();
+        $tailleTactique = strlen($tactique);
+        if ($tailleTactique < 50){
+            $pdf->Cell(188,40,utf8_decode("Qualité Tactique"),1,0,'D',0);
+            $pdf->MultiCell(288,40, utf8_decode($tactique),1,'D',0);
+        }else{
+            $pdf->Cell(188,40,utf8_decode("Qualité Tactique"),1,0,'D',0);
+            $pdf->MultiCell(288,23, utf8_decode($tactique),1,'D',0);
+        }
+
+        $pdf->Ln();
+        $pdf->SetX(70);
+
+        $mental = $rapportSpecifique->getQualiteMentale();
+        $tailleMental = strlen($mental);
+        if ($tailleMental < 50){
+            $pdf->Cell(188,40,utf8_decode("Qualité Mental"),1,0,'D',0);
+            $pdf->MultiCell(288,40, utf8_decode($mental),1,'D',0);
+        }else{
+            $pdf->Cell(188,40,utf8_decode("Qualité Mental"),1,0,'D',0);
+            $pdf->MultiCell(288,23, utf8_decode($mental),1,'D',0);
+        }
+        $pdf->Ln();
+
+        $pdf ->Output(); //génère le PDF et l'affiche
+        die("");
     }
 }
